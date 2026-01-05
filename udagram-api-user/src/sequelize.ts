bd9bba9d54bdbebe802b728f4974b6d34e2da0c3
phani5757 <59651098+phani5757@users.noreply.github.com>
2026-01-05 12:13:32 +0000

    Updated ci changes.

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,5 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
+import { Dialect } from 'sequelize';
 
 
 export const sequelize = new Sequelize({
@@ -8,7 +9,7 @@ export const sequelize = new Sequelize({
   'database': config.database,
   'host': config.host,
 
-  'dialect': config.dialect,
+  'dialect': config.dialect as Dialect,
   dialectOptions: {
     ssl: {
       require: true,
