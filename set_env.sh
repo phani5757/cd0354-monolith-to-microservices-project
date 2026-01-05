286bdb03a847d945daac8f2f20880765408bc2e2
phani <phani@test.com>
2026-01-05 11:26:55 +0530

    converted to micro service.

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres_09
+export POSTGRES_HOST=database-1.csjlzi8eaygr.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mybucket1200798
+export AWS_BUCKET=img-processing-581304011193-project
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
